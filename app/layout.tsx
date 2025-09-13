9625381404b5f8d329c4d1252ebbdbe981ee8b09
CryptoGuyDeve <m.faizurrehman.crypto@gmail.com>
2025-09-13 12:34:22 +0500

    03 Added Analytics from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import ConvexClientProvider from "@/components/ConvexClientProvider";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
           signUpFallbackRedirectUrl="/dashboard"
         >
           <ConvexClientProvider>{children}</ConvexClientProvider>
+          <Analytics />
         </ClerkProvider>
       </body>
     </html>
